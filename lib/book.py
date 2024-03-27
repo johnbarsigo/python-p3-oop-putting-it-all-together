1ff871309dff77ebf9a0deddec5a158977b17f2a
john.barsigo@student.moringaschool.com <john.barsigo@student.moringaschool.com>
2024-03-27 15:04:55 +0300

    Completed

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,39 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+
+    def __init__( self, title, page_count ) :
+
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def page_count ( self ) :
+
+        return self._page_count
     
+    # @property
+    # def title ( self ) :
+
+    #     return self._title
+    
+    @page_count.setter
+    def page_count ( self, value ) :
+
+        if isinstance ( value, int ) :
+
+            self._page_count = value
+        else :
+            
+            print ( "page_count must be an integer" )
+
+    
+    # @title.setter
+    # def page_count ( self, value ) :
+
+    #     self._title = value
+
+
+    def turn_page ( self ) :
         
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,46 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+
+    def __init__( self, brand, size ) :
+
+        self.brand = brand
+        self.size = size
+
+        # @property
+        # def brand ( self ) :
+
+        #     return self._brand
+        
+        @property
+        def size ( self ) :
+
+            return self._size
+        
+        
+        # @brand.setter
+        # def brand ( self, value ) :
+            
+        #     self._brand = value
+
+        
+        @size.setter
+        def size ( self, value ) :
+
+            if isinstance ( value, int ) :
+
+                self._size = value
+            else :
+                print ( "size must be an integer" )
+
+
+    def can_cobble ( self ) :
+
+        return ( "The shoe has been repaired." )
+        # print ( "Your shoe is as good as new!\n" )
+    
+    def cobble_makes_new ( self ) :
+
+        self.condition = "New"
+        # self = Shoe ( self.brand, self.size, self.condition )
+    
